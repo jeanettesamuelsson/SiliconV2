09d69490e89300a8a59e42fd4e488d316db72630
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2025-09-30 10:31:11 +0200

    .

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
+
 //Accordion
 
-const accordion = document.querySelectorAll('.accordion');
+const accordions = document.querySelectorAll(".accordion");
 
-accordion.forEach(accordion => {
-    accordion.addEventListener('click', () => {
-        
-        accordion.classList.toggle('active');
-    
-        const answer = accordion.nextElementSibling;
-        
-        if (answer.style.display === "block") {
-            answer.style.display = "none";
-        } else {
-            answer.style.display = "block";
+accordions.forEach(accordion => {
+    accordion.addEventListener("click", () => {
+
+        accordions.forEach(otherAcc => {
+
+        if (otherAcc !==accordion) {
+            otherAcc.classList.remove("active");
+            otherAcc.nextElementSibling.style.display ="none";
         }
+      });
+
+         accordion.classList.toggle("active");
+
+        const answer = accordion.nextElementSibling;
+
+        if (answer.style.display ==="block") {
+        answer.style.display = "none";
+        } else {
+        answer.style.display = "block";
+        }
+
     });
 });
-
 
 //Darkmode hantering
 
